10e8604ca539adf27ec2c44dab5190bdde6c0f8e
HAYOUNG LEE <l4538@naver.com>
2020-06-18 00:08:54 +0900

    add REST API

--- src/main/java/com/example/spring_project_1/domain/Restaurant.java
+++ src/main/java/com/example/spring_project_1/domain/Restaurant.java
@@ -3,10 +3,10 @@ package com.example.spring_project_1.domain;
 public class Restaurant {
     private final String name;
     private String addr;
-    public Restaurant(String name) {
-        this.name = name;
-    }
-    public Restaurant(String name, String addr) {
+    private final Long id;
+
+    public Restaurant(Long id, String name, String addr) {
+        this.id = id;
         this.name = name;
         this.addr = addr;
     }
---
+++ src/test/java/com/example/spring_project_1/inferfaces/RestaurantControllerTest.java
@@ -0,0 +1,30 @@
+package com.example.spring_project_1.inferfaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+
+@WebMvcTest(RestaurantController.class)
+class RestaurantControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+    @Test
+    public void list() throws Exception {
+        mvc.perform(get("/restaurants"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("BOB")));
+    }
+
+}
